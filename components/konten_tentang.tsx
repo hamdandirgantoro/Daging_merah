3ab9ecb42fd39d3b2f3f33f9eb77e155ffe8aa3f
hamdan <hamdandirgantara@gmail.com>
2022-12-15 14:10:11 +0700

    update about

--- components/konten_tentang.tsx
+++ components/konten_tentang.tsx
@@ -7,7 +7,13 @@ export default function KontenTentang() {
       style={{ zIndex: 1 }}
     >
       <div className="rounded p-3 h-fit w-fit bg-white flex justify-center items-center">
-        test
+        PT.Daging Merah adalah perusahaan yang berdiri sejak tahun 1990 yang
+        berfokus pada industri pangan khususnya daging sapi, <br />
+        kami berkomitmen untuk menghadirkan solusi bahan pangan yang berkualitas
+        dan terjangkau untuk masyarakat di indonesia <br />
+        PT.Daging Merah bercita-cita untuk menyediakan daging sapi ke semua
+        kalanganan masyarakat di indonesia sehingga <br />
+        daging sapi bukan hanya bisa di nikmati oleh kalangan atas saja
       </div>
     </div>
   );
